aaafc8d78c31e8655aa75063856ae6984010a81b
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-18 15:56:41 +0600

    minor changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         <div>
             <div className="banner mb-5">
-                <div className="w-75">
+                <div className="w-75 text-primary">
                     <h1 className='fw-bolder'>Dr. Lisa Nolan</h1>
                     <h5 className='fw-bolder'>Lisa Nolan's Clinic Welcomes you</h5>
                     <h5 className='fw-bolder'>Hope you are fine and best wishes for services</h5>
