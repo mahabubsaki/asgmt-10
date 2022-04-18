30f9214761a24155d2c10ed6c366211ffcacfa10
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-18 13:59:12 +0600

    Environment variable done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyACkKsMKKHWzEBoi67gYLrhKjglmn4SXlg",
-    authDomain: "medispace-416d4.firebaseapp.com",
-    projectId: "medispace-416d4",
-    storageBucket: "medispace-416d4.appspot.com",
-    messagingSenderId: "175710617101",
-    appId: "1:175710617101:web:523143e132bea727595f44"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
